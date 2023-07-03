f5567df6b8c93e5c4b09daec77023925054723de
AgudaD <agudaoluwanifemi@gmail.com>
2023-07-03 17:57:42 +0100

    new commit

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -1,25 +1,39 @@
 import { useState, useEffect } from "react";
 import config from "../config";
 
-
 const API = () => {
+  const [popular, setPopular] = useState([]);
 
   useEffect(() => {
     getPopular();
-  }, [])
+  }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${config.apiKey}`
-    );
+    const check = localStorage.getItem("popular");
 
-    const data = await api.json();
-    console.log(data);
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${config.apiKey}&number=10`
+      );
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      const data = await api.json();
+      setPopular(data.recipes);
+    }
   };
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-center space-y-5 p-4">
-      yeah, hello
+    <div className="flex min-h-screen w-screen flex-col items-center space-y-5 p-4">
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <img src={recipe.image} alt={recipe.title} />
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -1,82 +1,101 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MinusCircle, PlusCircle, X } from "phosphor-react";
-import axios from "axios";
-import beefBurger from "../assets/beefBurger.jpg";
+import config from "../config";
 
-const meals = [
-  {
-    id: 1,
-    name: "Burger",
-    price: 8.61,
-    image:
-      "https://www.transparentpng.com/thumb/burger/hot-burger-king-hd-png-9dzyCE.png",
-  },
-  {
-    id: 2,
-    name: "Tacos",
-    price: 3.49,
-    image:
-      "https://deltaco.com/files/menu/item/DEL-276_Promo-5-Website-Images_Hero_The-Del-Taco_860x480.png?v=4.027",
-  },
-  {
-    id: 3,
-    name: "Pizza",
-    price: 17.81,
-    image:
-      "https://d257c1zjbj9yqq.cloudfront.net/general-uploads/Menu-Images/_1200x630_crop_center-center_82_none/Bronco.png?mtime=1617398245",
-  },
-  {
-    id: 4,
-    name: "Chicken Wings",
-    price: 20,
-    image: "https://pngimg.com/d/kfc_food_PNG40.png",
-  },
-  {
-    id: 5,
-    name: "Sandwich",
-    price: 7.59,
-    image: "https://www.menudrive.com/wp-content/uploads/2021/02/Sandwich.png",
-  },
-  {
-    id: 6,
-    name: "Shawarma",
-    price: 10,
-    image:
-      "https://doner-chi.ca/wp-content/uploads/2022/04/Turkish-Shawarma.png",
-  },
-  {
-    id: 7,
-    name: "Hotdog",
-    price: 3.95,
-    image:
-      "https://assets.website-files.com/603372273fbc1306f1583eab/60364be6e775f2349bb6dbad_cheerleaders-hotdogs-got-the-flava.png",
-  },
-  {
-    id: 8,
-    name: "Salad",
-    price: 4.25,
-    image:
-      "https://images.ctfassets.net/l5fkpck1mwg3/7DzLQK4fxXHEaMRLjS6Taf/c9ad552a443f0af7888222b3738cc417/Chicken_Caesar_Salad.png",
-  },
-  {
-    id: 9,
-    name: "Milkshake",
-    price: 7.29,
-    image:
-      "https://zalimfoods.com/wp-content/uploads/2023/03/New-Project-45.png",
-  },
-  {
-    id: 10,
-    name: "Fries",
-    price: 5,
-    image:
-      "https://images.ctfassets.net/l5fkpck1mwg3/5Ih3U85mGJXFzfQrfe9yP4/334041e07865621f88f687c6a5291463/Appetizers_French_Fries.png",
-  },
-];
+// const meals = [
+//   {
+//     id: 1,
+//     name: "Burger",
+//     price: 8.61,
+//     image:
+//       "https://www.transparentpng.com/thumb/burger/hot-burger-king-hd-png-9dzyCE.png",
+//   },
+//   {
+//     id: 2,
+//     name: "Tacos",
+//     price: 3.49,
+//     image:
+//       "https://deltaco.com/files/menu/item/DEL-276_Promo-5-Website-Images_Hero_The-Del-Taco_860x480.png?v=4.027",
+//   },
+//   {
+//     id: 3,
+//     name: "Pizza",
+//     price: 17.81,
+//     image:
+//       "https://d257c1zjbj9yqq.cloudfront.net/general-uploads/Menu-Images/_1200x630_crop_center-center_82_none/Bronco.png?mtime=1617398245",
+//   },
+//   {
+//     id: 4,
+//     name: "Chicken Wings",
+//     price: 20,
+//     image: "https://pngimg.com/d/kfc_food_PNG40.png",
+//   },
+//   {
+//     id: 5,
+//     name: "Sandwich",
+//     price: 7.59,
+//     image: "https://www.menudrive.com/wp-content/uploads/2021/02/Sandwich.png",
+//   },
+//   {
+//     id: 6,
+//     name: "Shawarma",
+//     price: 10,
+//     image:
+//       "https://doner-chi.ca/wp-content/uploads/2022/04/Turkish-Shawarma.png",
+//   },
+//   {
+//     id: 7,
+//     name: "Hotdog",
+//     price: 3.95,
+//     image:
+//       "https://assets.website-files.com/603372273fbc1306f1583eab/60364be6e775f2349bb6dbad_cheerleaders-hotdogs-got-the-flava.png",
+//   },
+//   {
+//     id: 8,
+//     name: "Salad",
+//     price: 4.25,
+//     image:
+//       "https://images.ctfassets.net/l5fkpck1mwg3/7DzLQK4fxXHEaMRLjS6Taf/c9ad552a443f0af7888222b3738cc417/Chicken_Caesar_Salad.png",
+//   },
+//   {
+//     id: 9,
+//     name: "Milkshake",
+//     price: 7.29,
+//     image:
+//       "https://zalimfoods.com/wp-content/uploads/2023/03/New-Project-45.png",
+//   },
+//   {
+//     id: 10,
+//     name: "Fries",
+//     price: 5,
+//     image:
+//       "https://images.ctfassets.net/l5fkpck1mwg3/5Ih3U85mGJXFzfQrfe9yP4/334041e07865621f88f687c6a5291463/Appetizers_French_Fries.png",
+//   },
+// ];
 
 const Shops = () => {
-  const [count, setCount] = useState(0);
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${config.apiKey}&number=10`
+      );
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      const data = await api.json();
+      setPopular(data.recipes);
+    }
+  };
 
   return (
     <div>
@@ -90,27 +109,29 @@ const Shops = () => {
         </div>
       </div>
 
-      {meals.map((meal) => (
-        <div key={meal.id} className="mt-10 px-6">
-          <div className="mb-10 flex items-center justify-between rounded-md p-4 shadow-md drop-shadow-md">
-            <div className="flex items-center gap-5">
-              <img src={meal.image} alt="" className="w-[10rem]" />
-              <div>
-                <p>{meal.name}</p>
-                <p>${meal.price}</p>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id} className="mt-10 px-6">
+            <div className="mb-10 flex items-center justify-between rounded-md p-4 shadow-md drop-shadow-md">
+              <div className="flex items-center gap-5">
+                <img src={recipe.image} alt="" className="w-[10rem]" />
+                <div>
+                  <p>{recipe.name}</p>
+                  {/* <p>${recipe.price}</p> */}
+                </div>
+              </div>
+
+              <div className="flex max-w-[7rem] items-center justify-between gap-4">
+                <button className="rounded-full border border-[#e91e63] px-8 py-1.5 hover:bg-[#e91e6248]">
+                  Add
+                </button>
               </div>
             </div>
-
-            <div className="flex max-w-[7rem] items-center justify-between gap-4">
-              <button className="rounded-full border border-[#e91e63] px-8 py-1.5 hover:bg-[#e91e6248]">
-                Add
-              </button>
-            </div>
           </div>
-        </div>
-      ))}
-      <div className="flex items-center justify-center mb-12">
-        <button className="rounded-full bg-[#e91e63] px-16 py-1.5 hover:bg-[#e91e62cb] text-white">
+        );
+      })}
+      <div className="mb-12 flex items-center justify-center">
+        <button className="rounded-full bg-[#e91e63] px-16 py-1.5 text-white hover:bg-[#e91e62cb]">
           Proceed
         </button>
       </div>
